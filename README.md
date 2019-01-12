140fb9c0c3e199a7029307cbf4595ed6dd1e4efb
sudaraka <sudaraka@circles.asia>
2019-01-12 21:07:33 +0800

    Initial commit :tada:

---
+++ README.md
@@ -0,0 +1,2 @@
+## Discourse Donor Box Plugin
+A plugin to sync discourse with Donor Box.
---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: discourse-donorbox-plugin
+# about: A plugin to sync discourse with Donor Box
+# version: 0.1
+# authors: Sudaraka Jayathilaka
+# url: https://github.com/sudaraka94/discourse-donorbox-plugin
