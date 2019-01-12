ad8d1a0f9b96f958ca140d6390a058c9a5c80eeb
sudaraka <sudaraka@circles.asia>
2019-01-12 21:25:24 +0800

    Add enabled settings

--- plugin.rb
+++ plugin.rb
@@ -3,3 +3,13 @@
 # version: 0.1
 # authors: Sudaraka Jayathilaka
 # url: https://github.com/sudaraka94/discourse-donorbox-plugin
+
+enabled_site_setting :donorbox_enabled
+
+module ::OpencollectivePlugin
+  
+end
+
+after_initialize do
+
+end
