98cbd6a0294e9feebe60c3e673c26b7529c09f9b
sudaraka <sudaraka@circles.asia>
2019-01-15 00:40:04 +0800

    Create and grant the badge

--- plugin.rb
+++ plugin.rb
@@ -7,7 +7,16 @@
 enabled_site_setting :donorbox_enabled
 
 module ::OpencollectivePlugin
-  
+  BADGE_NAME ||= SiteSetting.donorbox_badge_name.freeze
+
+  def self.badges_grant!(user)
+    unless badge = Badge.find_by(name: BADGE_NAME)
+      badge = Badge.create!(name: BADGE_NAME,
+                           description: SiteSetting.donorbox_badge_description,
+                           badge_type_id: 1)
+    end
+    BadgeGranter.grant(badge, user)
+  end
 end
 
 after_initialize do
